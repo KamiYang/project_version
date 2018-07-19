2caa07583cee70b8247f36a5b1483a9ef53c8ee2
kamiyang <jan.stockfisch@googlemail.com>
2018-07-19 16:42:12 +0200

    [TASK] Raise version number and set state to "beta"

--- ext_emconf.php
+++ ext_emconf.php
@@ -4,17 +4,17 @@ $EM_CONF[$_EXTKEY] = [
     'title' => 'Project Version',
     'description' => 'Displays current project version based on \'VERSION\' file or GIT revision.',
     'category' => 'misc',
-    'state' => 'alpha',
+    'state' => 'beta',
     'uploadfolder' => 0,
     'createDirs' => '',
     'clearCacheOnLoad' => 1,
     'author' => 'Jan Stockfisch',
     'author_email' => 'jan.stockfisch@googlemail.com',
-    'version' => '0.3.0',
+    'version' => '0.3.1',
     'constraints' => [
         'depends' => [
             'php' => '7.0',
-            'typo3' => '8.7.0-8.7.16'
+            'typo3' => '8.7.0-8.7.17'
         ],
         'conflicts' => [
         ],
